cca524de56c37e4ae8d48cb25e00470fb84ab298
David Podhola <david.podhola@naseukoly.cz>
2016-05-08 16:56:41 +0200

    project scaffold added

---
+++ appblockmssql/tests/ApplicationName.Core.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module ApplicationName.Core.Tests
+
+open ApplicationName.Core
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
