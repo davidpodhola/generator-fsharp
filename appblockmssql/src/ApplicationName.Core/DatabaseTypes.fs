29058fbb5454c0cc8eaa792f173c7427b66667ff
David Podhola <david.podhola@naseukoly.cz>
2016-05-08 20:47:29 +0200

    DatabaseTypes.fs

---
+++ appblockmssql/src/ApplicationName.Core/DatabaseTypes.fs
@@ -0,0 +1,8 @@
+﻿module DatabaseTypes
+
+[<Literal>]
+let connectionStringForCompileTime = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=" + __SOURCE_DIRECTORY__ + @"\tools\Database1.mdf;Integrated Security=True;Connect Timeout=10"
+
+type Foo = SqlCommandProvider<"SELECT * FROM Foo", connectionStringForCompileTime>
+
+let myResults = (new Foo("Use your Runtime connectionString here")).Execute()
--- appblockmssql/src/ApplicationName.Core/tools/generateSchemaSQL.bat
+++ appblockmssql/src/ApplicationName.Core/tools/generateSchemaSQL.bat
@@ -1,4 +1,3 @@
 DBME_CopyToFile  -f "..\DatabaseTables.xlsm" -m "CopyAllCreateTableIfNotExistsSQL" -o "..\Schema.sql"
 rem @powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden createdb.ps1
 @powershell -ExecutionPolicy Bypass ./createdb.ps1
-pause
