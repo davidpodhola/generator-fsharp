94f95f7efeb5f75318e3d3201b3d24fbfd13debb
David Podhola <david.podhola@naseukoly.cz>
2016-05-08 17:03:29 +0200

    just compile + tests

--- appblockmssql/src/ApplicationName.Core/AssemblyInfo.fs
+++ appblockmssql/src/ApplicationName.Core/AssemblyInfo.fs
@@ -3,11 +3,8 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("ApplicationName.Core")>]
 [<assembly: AssemblyProductAttribute("ApplicationName.Core")>]
-[<assembly: AssemblyDescriptionAttribute("XXX")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
-    let [<Literal>] InformationalVersion = "1.0"
+    let [<Literal>] Version = "LocalBuild"
+    let [<Literal>] InformationalVersion = "LocalBuild"
