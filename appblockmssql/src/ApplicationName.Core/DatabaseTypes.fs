4cb85ec7a2ead88837158ada0770936650d5b2af
David Podhola <david.podhola@naseukoly.cz>
2016-05-08 21:20:52 +0200

    buildable on windows in visual studio
    (build.cmd not building anything)

--- appblockmssql/src/ApplicationName.Core/DatabaseTypes.fs
+++ appblockmssql/src/ApplicationName.Core/DatabaseTypes.fs
@@ -4,6 +4,7 @@ open FSharp.Data
 [<Literal>]
 let connectionStringForCompileTime = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=" + __SOURCE_DIRECTORY__ + @"\tools\Database1.mdf;Integrated Security=True;Connect Timeout=10"
 
-type Foo = SqlCommandProvider<"SELECT * FROM Employees", connectionStringForCompileTime>
+type EmployeesQuery = SqlCommandProvider<"SELECT * FROM Employees", connectionStringForCompileTime>
 
-let myResults = (new Foo("Use your Runtime connectionString here")).Execute()
+let myResults = (new EmployeesQuery("Use your Runtime connectionString here")).Execute()
+
