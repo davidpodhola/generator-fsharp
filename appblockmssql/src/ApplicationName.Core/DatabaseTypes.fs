a7119ac38421a25160ea1ef269915d4a5cd5f1bc
David Podhola <david.podhola@naseukoly.cz>
2016-05-09 07:54:21 +0200

    DatabaseTypes.fs has #if DEMO too

--- appblockmssql/src/ApplicationName.Core/DatabaseTypes.fs
+++ appblockmssql/src/ApplicationName.Core/DatabaseTypes.fs
@@ -4,7 +4,9 @@ open FSharp.Data
 [<Literal>]
 let connectionStringForCompileTime = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=" + __SOURCE_DIRECTORY__ + @"\tools\Database1.mdf;Integrated Security=True;Connect Timeout=10"
 
+#if DEMO
 type EmployeesQuery = SqlCommandProvider<"SELECT * FROM Employees", connectionStringForCompileTime>
 
-let myResults = (new EmployeesQuery("Use your Runtime connectionString here")).Execute()
+let allEmployees = (new EmployeesQuery("RuntimeConnectionString")).Execute()
+#endif
 
